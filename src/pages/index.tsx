3da3602291283e569f3bf3707de9c6e7d1371abe
Kandimalla Harsha Vardhan <kandimalla.11905169@lpu.in>
2022-09-14 23:05:56 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,8 +13,8 @@ export default function Home() {
   return (
     <Page>
       <NextSeo
-        title="STARTD - Template"
-        description="A TypeScript/Next.js theme that includes everything you need to build amazing landing page!"
+        title="makeitdecentral - the future of blockchain based payments"
+        description="the future of blockchain based web and app payments powered by polygon."
       />
       <Header />
       <main>
